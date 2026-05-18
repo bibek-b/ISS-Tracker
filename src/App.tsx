86bec39a8eebba59c06948579b915d0335b9dfe4
Bibek Gadel <vieekgadel49@gmail.com>
2026-05-18 21:16:39 +0545

    remove unused codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import ISSTracker from "./pages/ISSTracker"
-import Navbar from "./components/Navbar"
-import AboutISS from "./pages/AboutIss"
-import Astronauts from "./pages/AstronautsInfo"
-import LiveView from "./pages/CameraView"
-import CameraView from "./pages/CameraView"
-import AstronautsInfo from "./pages/AstronautsInfo"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ISSTracker from "./pages/ISSTracker";
+import Navbar from "./components/Navbar";
+import AboutISS from "./pages/AboutIss";
+import CameraView from "./pages/CameraView";
+import AstronautsInfo from "./pages/AstronautsInfo";
 
 const App = () => {
   return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ISSTracker />} />
+        <Route path="/cameraView" element={<CameraView />} />
+        <Route path="/astronautsInfo" element={<AstronautsInfo />} />
+        <Route path="/aboutIss" element={<AboutISS />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-  //  <>
-  //  {/* <ISSTracker /> */}
-  //  {/* <LiveView /> */}
-  //  {/* <Astronauts /> */}
-  //  <AboutISS />
-  //  </>
-  
-  <BrowserRouter>
-  <Navbar />
-  <Routes>
-    <Route path="/" element={<ISSTracker />} />
-    <Route path="/cameraView" element={<CameraView />} />
-    <Route path="/astronautsInfo" element={<AstronautsInfo />} />
-    <Route path="/aboutIss" element={<AboutISS />} />
-  </Routes>
-  </BrowserRouter>
-  )
-}
-
-export default App
+export default App;

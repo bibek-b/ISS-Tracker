3bebfbb04c933a750aa7c4961a0e110380bdb064
Bibek Gadel <vieekgadel49@gmail.com>
2026-05-19 15:40:45 +0545

    learnTerms path added

--- src/constants/navbar.ts
+++ src/constants/navbar.ts
@@ -1,5 +1,6 @@
 export const NAVBAR_OPTIONS = [
     {id:1, label: "Camera View", path: "/cameraView"},
     {id:2, label: "Astronauts Info", path: "/astronautsInfo"},
-    {id:2, label: "About ISS", path: "/aboutIss"}
+    {id:3, label: "About ISS", path: "/aboutIss"},
+    {id:4, label: "Learn Terms", path: "/learnTerms"}
 ]
